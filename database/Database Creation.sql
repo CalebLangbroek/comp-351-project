d7bb56c47d53acbbd964a5cb455cc5fb8f932981
CalebLangbroek <34527287+CalebLangbroek@users.noreply.github.com>
2021-03-13 12:58:02 -0800

    Adding database files

---
+++ database/Database Creation.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE comp_351_project;
+
+CREATE TABLE `question` (
+    `question_id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `text` VARCHAR( 255 ) NOT NULL UNIQUE KEY
+);
+
+CREATE TABLE `choice` (
+    `choice_id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `question_id` INT( 11 ) NOT NULL,
+    `text` VARCHAR( 255 ) NOT NULL UNIQUE KEY,
+    `index` INT( 11 ) NOT NULL,
+    CONSTRAINT fk_question FOREIGN KEY ( question_id ) REFERENCES question(question_id)
+);
+
+CREATE TABLE `answer` (
+    `answer_id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `question_id` INT( 11 ) NOT NULL,
+    `choice_id` INT( 11 ) NOT NULL,
+    CONSTRAINT fk_answerquestion FOREIGN KEY ( question_id ) REFERENCES question(question_id),
+    CONSTRAINT fk_answerchoice FOREIGN KEY ( choice_id ) REFERENCES choice(choice_id)
+);
---
+++ database/Test Data Creation.sql
@@ -0,0 +1,5 @@
+INSERT INTO question (text) VALUES ("This is a test question");
+
+
+-- Cleans table
+TRUNCATE TABLE question;
