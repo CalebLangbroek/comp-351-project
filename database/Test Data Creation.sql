a20d9a983f87782ede3cfb8cba20153a5644f49f
CalebLangbroek <34527287+CalebLangbroek@users.noreply.github.com>
2021-03-20 00:19:26 -0700

    Removing un-needed DB files

--- database/Test Data Creation.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO question (question_id, text) VALUES (1, 'What is 4+4?');
-INSERT INTO choice (`question_id`, `text`) VALUES (1, '8');
-INSERT INTO choice (`question_id`, `text`) VALUES (1, '34');
-INSERT INTO choice (`question_id`, `text`) VALUES (1, '4');
-INSERT INTO choice (`question_id`, `text`) VALUES (1, '2');
-INSERT INTO answer (question_id, choice_id) VALUES (1, 1)
-
--- Cleans table
-TRUNCATE TABLE question;
--- database/User Creation.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'comp-351-project'@'localhost' IDENTIFIED BY 'password';
-
-GRANT DELETE ON comp_351_project.* TO 'comp-351-project'@'localhost';
-GRANT INSERT ON comp_351_project.* TO 'comp-351-project'@'localhost';
-GRANT SELECT ON comp_351_project.* TO 'comp-351-project'@'localhost';
-GRANT UPDATE ON comp_351_project.* TO 'comp-351-project'@'localhost';
-
-SHOW GRANTS FOR 'comp-351-project'@'localhost';
